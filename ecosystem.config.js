5f7a1e71a149a93682aef40ee953d76e762a09d1
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-01-30 02:16:30 +0100

    :construction: Implement ecosystem.config.ts

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nuxt-ts-app',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+    },
+  ],
+}
